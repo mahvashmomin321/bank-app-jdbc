961d6b39872f20fbd5d82a65e38e5f7b12d4b85c
Momin <mahvash-nazir.momin@capgemini.com>
2019-03-28 19:10:11 +0530

    files added

---
+++ src/com/cagemini/bankapp/exception/BankAccountNotFoundException.java
@@ -0,0 +1,10 @@
+package com.cagemini.bankapp.exception;
+
+public class BankAccountNotFoundException extends Exception {
+
+	public BankAccountNotFoundException(String message) {
+		super(message);
+		
+	}
+
+}
